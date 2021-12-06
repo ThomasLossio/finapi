92d308752555d6d182ca85e1ff1ac46ca6def884
Thomas Lossio <thomaslossio@hotmail.com>
2021-12-06 13:46:48 -0300

    Add deposit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 const customers = []
 
 function verifyIfExistsAccountCPF(request, response, next) {
-  const { cpf } = request.params
+  const { cpf } = request.headers
 
   const customer = customers.find(customer => customer.cpf === cpf)
 
@@ -41,10 +41,27 @@ app.post('/account', (request, response) => {
   return response.status(201).send()
 })
 
-app.get('/statement/:cpf', verifyIfExistsAccountCPF, (request, response) => {
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
   const { customer } = request
 
   return response.json(customer.statement)
+})
+
+app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description, 
+    amount, 
+    created_at: new Date(),
+    type: "credit",
+  }
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
 })
 
 app.listen(3333)
